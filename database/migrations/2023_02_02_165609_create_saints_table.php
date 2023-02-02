369cc0b6fdba520871fe1c9f4b2cc61c583fcf3e
sirio95 <sirio.iasevoli@gmail.com>
2023-02-02 17:59:13 +0100

    tabella santi - migrate

---
+++ database/migrations/2023_02_02_165609_create_saints_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('saints', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->string('name', 32);
+            $table->string('birth_place', 32);
+            $table->date('sactification_date');
+            $table->integer('num_miracles')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('saints');
+    }
+};
